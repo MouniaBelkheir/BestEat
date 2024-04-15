1b84f1d0862db92a440bfb393f407b1c22636dbf
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-04-15 23:59:00 +0100

    added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
-  return (
-    <h1 className='text-red-500'>
-      hi 
-    </h1>
-  )
+	return (
+		<div>
+			<Navbar />
+		</div>
+	);
 }
 
-export default App
+export default App;
