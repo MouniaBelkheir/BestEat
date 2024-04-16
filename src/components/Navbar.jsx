ca922d4b085211ed28a2823c7d9aad3db00197b7
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-04-16 16:39:25 +0100

    Added functionality to the left navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
-import { AiFillTag, AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
-import { BsFillCartFill, BsFillSave2Fill, BsFillSaveFill } from "react-icons/bs";
+import { useState } from "react";
+import {
+	AiFillTag,
+	AiOutlineClose,
+	AiOutlineMenu,
+	AiOutlineSearch,
+} from "react-icons/ai";
+import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
 import { FaUserFriends, FaWallet } from "react-icons/fa";
 import { MdFavorite, MdHelp } from "react-icons/md";
 import { TbTruckDelivery } from "react-icons/tb";
 
 const Navbar = () => {
+	const [nav, setNav] = useState(false);
 	return (
 		<div className="max-w-[1640px] mx-auto flex justify-between items-center p-4 ">
 			{/* Left side */}
 			<div className="flex items-center">
-				<div className="cursor-pointer ">
+				<div onClick={() => setNav(!nav)} className="cursor-pointer ">
 					<AiOutlineMenu size={30} />
 				</div>
 				<h1 className="text-2xl sm:text-3xl lg:text-4xl px-2 ">
@@ -36,10 +43,21 @@ const Navbar = () => {
 			</button>
 			{/* Mobile Menu */}
 			{/* Overlay */}
-			<div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+			{nav ? (
+				<div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+			) : (
+				""
+			)}
+
 			{/* Side drawer menu */}
-			<div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300 ">
+			<div
+				className={
+					nav
+						? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+						: "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+				}>
 				<AiOutlineClose
+					onClick={() => setNav(!nav)}
 					size={30}
 					className="absolute right-4 top-4 cursor-pointer"
 				/>
@@ -49,7 +67,7 @@ const Navbar = () => {
 				<nav>
 					<ul className="flex flex-col p-4 text-gray-800">
 						<li className="text-xl py-4 flex">
-							<TbTruckDelivery size={25} className="mr-4"/> Orders
+							<TbTruckDelivery size={25} className="mr-4" /> Orders
 						</li>
 						<li className="text-xl py-4 flex">
 							<MdFavorite size={25} className="mr-4" /> Favorites
